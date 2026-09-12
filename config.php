0cf77b602e79ecd69c30003772df37d32e3425d9
Gisoogholizade <gholizadegisoo@gmail.com>
2026-09-12 14:16:22 +0330

    Clean up production configuration

--- config.php
+++ config.php
@@ -3,10 +3,8 @@ return [
     'app_name' => 'کافه آکاتسوکی',
     'tagline' => 'یک جرعه آرامش در دل شب',
     'timezone' => 'Asia/Tehran',
+    // After deployment, set this to the final public menu URL.
     'menu_url' => '',
-    'admin_username' => 'admin',
-    // Change this before production. Default password: ChangeMe123!
-    'admin_password_hash' => '$2y$10$S1SYNr4sQJZl3K/Ve3j8juM7TBj0xY0j0QbpzB1U0bQK6xU2CCM5u',
     'currency' => 'تومان',
     'max_upload_mb' => 3,
 ];
